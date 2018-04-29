57290516e65f82455e61201b459a7ca1923f1e42
Vinay Varshney <vinay.varshney28@gmail.com>
2018-04-29 20:24:14 +0530

    First Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { WorldHttpService } from '../world-http.service';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(public _route : ActivatedRoute, public router : Router, private worldHttpService : WorldHttpService , private spinnerService: Ng4LoadingSpinnerService) { }
+
+  ngOnInit(){
+    this.spinnerService.show();
+    setTimeout(() => {
+      this.spinnerService.hide();
+    }, 2000);
+  }
+
+}
